74a715176ee05a600f4ce539a404ed738a17a72e
Chetan717 <chaitanyachaudhari717@gmail.com>
2024-07-29 16:50:22 +0530

    sdfsd

--- moduleProject/terrform.tf
+++ moduleProject/terrform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "4.66.1"
     }
   } 
+  backend "s3" {
+    bucket = "state_bucket7"
+    key = "terraform.tfstate"
+    region = "us-west-2"
+    dynamodb_table = "remotr_state_terra"
+  }
 }
